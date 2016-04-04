8190eb3166a26f65d8257e0879a863d742a33472
Dan Mutton <daniel.mutton@gmail.com>
2016-04-04 22:41:56 +1000

    Oh yeah... should also describe what's under test

--- test/memberController_test.js
+++ test/memberController_test.js
@@ -5,28 +5,28 @@ import assert from 'assert';
 
 describe('MemberController', function() {
 
+    describe('submitMember', function() {
+        it('should return true if there are no errors and it saves', function() {
+            Validator.validateMemberDetails = function() { return []; };
+            DatabaseService.prototype.save = function() { return true; };
 
-    it('should return true if there are no errors and it saves', function() {
-        Validator.validateMemberDetails = function() { return []; };
-        DatabaseService.prototype.save = function() { return true; };
+            var result = new MemberController().submitMember({});
+            assert.equal(result, true);
+        });
 
-        var result = new MemberController().submitMember({});
-        assert.equal(result, true);
+        it('should return false if there are validation errors', function() {
+            Validator.validateMemberDetails = function() { return ['error']; };
+
+            var result = new MemberController().submitMember({});
+            assert.equal(result, false);
+        });
+
+        it('should return false if there is a save error', function() {
+            Validator.validateMemberDetails = function() { return []; };
+            DatabaseService.prototype.save = function() { return false; };
+
+            var result = new MemberController().submitMember({});
+            assert.equal(result, false);
+        });
     });
-
-    it('should return false if there are validation errors', function() {
-        Validator.validateMemberDetails = function() { return ['error']; };
-
-        var result = new MemberController().submitMember({});
-        assert.equal(result, false);
-    });
-
-    it('should return false if there is a save error', function() {
-        Validator.validateMemberDetails = function() { return []; };
-        DatabaseService.prototype.save = function() { return false; };
-
-        var result = new MemberController().submitMember({});
-        assert.equal(result, false);
-    });
-
 });
